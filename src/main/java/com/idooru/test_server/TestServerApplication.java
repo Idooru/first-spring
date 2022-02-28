0e3298742ab143b9fb7b52511473d0008b0e4fd6
shere1765 <shere1765@gmail.com>
2022-02-28 21:17:40 +0900

    second spring

--- src/main/java/com/idooru/test_server/TestServerApplication.java
+++ src/main/java/com/idooru/test_server/TestServerApplication.java
@@ -8,6 +8,7 @@ public class TestServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestServerApplication.class, args);
+		System.out.println("Hello Spring!");
 	}
 
 }
